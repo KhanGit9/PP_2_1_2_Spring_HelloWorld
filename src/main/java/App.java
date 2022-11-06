ba9d089b20e1dda333579cf3f8d11a9bd54a2280
KhanGit9 <111582722+KhanGit9@users.noreply.github.com>
2022-11-06 14:21:06 +0300

    af

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+       HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("catBean");
+        System.out.println(cat.getMesseg());
+        Cat cat1 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(cat1.getMesseg());
+        System.out.println("Результат сравнения бинов helloworld = " +
+                bean.equals(bean2));
+        System.out.println("Результат сравнения биноа catBean = " +
+                cat.equals(cat1));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+
+    private String messeg;
+
+    public String getMesseg() {
+        return messeg;
+    }
+    public void setMesseg (String messeg) {
+        this.messeg = messeg;
+    }
+}
